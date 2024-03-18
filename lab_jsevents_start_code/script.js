046c41f3a5c2efc1709320553fdada3acf7818c6
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-18 16:00:25 +0000

    added grey out completed box

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,15 +1,28 @@
-const enter = document.querySelector("#todo-form")
+const enter = document.querySelector("#todo-form");
 const list = document.querySelector("#list");
-const selectButton = document.querySelector("#show-date")
+const selectButton = document.querySelector("#show-date");
+const completedList = document.querySelector("#completed-list");
 
 enter.addEventListener("submit", (event) => {
     event.preventDefault();
     const newItem = document.createElement("li");
-    const deleteButton = document.createElement("button");
+    const deleteButton = document.createElement("button")
+    const checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
     newItem.innerText = event.target["new-todo"].value;
     deleteButton.innerText = "delete " + event.target["new-todo"].value;
+    list.appendChild(checkbox);
     list.appendChild(newItem);
     list.appendChild(deleteButton);
+    checkbox.addEventListener("click", () =>{
+        if (checkbox.checked){
+            console.log("checked")
+            newItem.style.color = "grey"
+        } else {
+            newItem.style.color = "black"
+        }
+        console.log(checkbox)
+    })
     deleteButton.addEventListener("click", () => {
         list.removeChild(newItem);
         list.removeChild(deleteButton);
